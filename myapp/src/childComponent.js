c74deaeb4c73b1d39d81fe58d8e1b61538f1de50
W A D G H Athukorala <gimanthaheshan2000@gmail.com>
2024-11-23 00:00:58 +0530

    add: child component in to App.js

--- myapp/src/childComponent.js
+++ myapp/src/childComponent.js
@@ -4,6 +4,7 @@ export default function Childcom(props){
         <h3>Hello React I'm Child</h3>
         {
             props.setfun("My Name is Gima")
+            props.setfun("My name is Gima")
         }
         </>
     );
